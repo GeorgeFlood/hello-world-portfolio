e44e06cc84d52d767f6d02a565f5303617be4822
GeorgeFlood <mrgeorgeflood@gmail.com>
2022-08-09 06:38:19 +0100

    second commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const faders = document.querySelectorAll(".fade-in");
+const header = document.querySelector(".header__nav");
+const sectionOne = document.querySelector(".header");
+const mobileMenu = document.querySelector(".header__mobile--icon");
+
+const sectionOneOptions = {
+  rootMargin: "-240px 0px -0px 0px",
+};
+
+const sectionOneObserver = new IntersectionObserver(function (
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav--scroll");
+    } else {
+      header.classList.remove("nav--scroll");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
+
+const appearOptions = {
+  threshold: 1,
+  rootMargin: "0px 0px -30px 0px",
+};
+
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader);
+});
+
+mobileMenu.addEventListener("click", (e) => {
+  mobileMenu.classList.toggle("is-active");
+  document.querySelector(".header__mobile--nav").classList.toggle("is-active");
+});
