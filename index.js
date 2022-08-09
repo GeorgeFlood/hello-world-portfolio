f8ff3003d0a084ac9f514fcbc42659a1f1d0a1b1
GeorgeFlood <mrgeorgeflood@gmail.com>
2022-08-09 10:01:58 +0100

    javascript'd the mobile menu

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const faders = document.querySelectorAll(".fade-in");
 const header = document.querySelector(".header__nav");
 const sectionOne = document.querySelector(".header");
 const mobileMenu = document.querySelector(".header__mobile--icon");
+const mobileMenuBtn = document.querySelector(".header__mobile--nav");
 
 const sectionOneOptions = {
   rootMargin: "-240px 0px -0px 0px",
@@ -50,4 +51,19 @@ faders.forEach((fader) => {
 mobileMenu.addEventListener("click", (e) => {
   mobileMenu.classList.toggle("is-active");
   document.querySelector(".header__mobile--nav").classList.toggle("is-active");
+});
+
+mobileMenuBtn.addEventListener("click", (e) => {
+  const target = e.target.classList;
+
+  const isActive = document.querySelector(".is-active");
+  if (
+    target.contains("header__mobile--items") ||
+    target.contains("is-active")
+  ) {
+    mobileMenu.classList.toggle("is-active");
+    document
+      .querySelector(".header__mobile--nav")
+      .classList.toggle("is-active");
+  }
 });
