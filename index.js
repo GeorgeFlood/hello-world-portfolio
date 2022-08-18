bf53bd3f068a8330a18f376a90d8f5b572da76ef
GeorgeFlood <mrgeorgeflood@gmail.com>
2022-08-18 23:18:20 +0100

    mobile fixes--

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const mobileMenu = document.querySelector(".header__mobile--icon");
 const mobileMenuBtn = document.querySelector(".header__mobile--nav");
 const mobileMenuBG = document.querySelector(".header__mobile--nav");
 
+//////////////////////
+///contact form elements
+const sendBtn = document.querySelector("#send");
+const contactTitle = document.getElementById("contactID");
+const contactName = document.getElementById("name");
+const contactEmail = document.getElementById("email");
+const contactMessage = document.getElementById("message");
+//////////////////////
+//nav bar
+
 const sectionOneOptions = {
   rootMargin: "-240px 0px -0px 0px",
 };
@@ -29,6 +39,9 @@ const appearOptions = {
   threshold: 1,
   rootMargin: "0px 0px -30px 0px",
 };
+
+//////////////////////
+///lazy animations
 
 const appearOnScroll = new IntersectionObserver(function (
   entries,
@@ -65,4 +78,31 @@ mobileMenuBtn.addEventListener("click", (e) => {
     mobileMenu.classList.toggle("is-active");
     mobileMenuBG.classList.toggle("is-active");
   }
+  return isActive;
+});
+
+////////////////////////////////////////////
+//contact form functions
+
+function sendMail() {
+  let params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+  emailjs
+    .send("service_87zdrt4", "template_q5c3dnf", params)
+    .then(function (res) {
+      alert("success " + res.status);
+    });
+}
+
+sendBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  sendMail();
+  contactTitle.classList.add("thank--you");
+  contactTitle.textContent = "Thank you!";
+  contactName.value = "";
+  contactEmail.value = "";
+  contactMessage.value = "";
 });
